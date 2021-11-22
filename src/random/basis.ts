f7ebce5dedc0e77a9668f7c2c9e70135f28b709e
grh-gitee <3494242908@qq.com>
2021-11-22 11:20:29 +0800

    update

--- src/random/basis.ts
+++ src/random/basis.ts
@@ -5,6 +5,24 @@ export function boolean(): boolean {
 }
 
 // id
-export function id():string {
-	return Math.random().toString().slice(2)	
+export function id(): string {
+	return Math.random().toString().slice(2)
+}
+
+
+
+/**
+ * @param {string} value uuid 的格式 default:'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx' 
+ * @returns {string} uuid
+ * @description 生成随机uid
+ */
+
+export function uuid(value: string): string {
+	let val: string = value || 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx';
+	let d: number = new Date().getTime();   // 随机种子
+	return val.replace(/[xy]/g, function (c: string): string {
+		let r: number = (d + Math.random() * 16) % 16 | 0;
+		d = Math.floor(d / 16);
+		return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+	});
 }
--- src/test/index.ts
+++ src/test/index.ts
@@ -14,6 +14,6 @@ console.log(
 	// _mock.mock({ 'nameX|2-4': '@name' }), '\n',
 	// _mock.mock({ 'nameX|0-2': '@name' }), '\n',
 	// _mock.mock({ 'nameX|2-4': '@name' }), '\n',
-	_mock.mock({ 'list|10-20': { 'a':'@name', id: '@id'} }), '\n',
+	_mock.mock({ 'list|10-20': { 'a': '@name', uid: '@uuid', id: '@id' } }), '\n',
 	// _mock.mock('@name'),
 );
